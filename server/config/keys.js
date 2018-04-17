a097875a4017d9e23d528ed89bb3f9365ad06c49
kayleabox <kayleabox09@gmail.com>
2018-04-17 17:14:33 -0500

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '189422841828-9u6c18q8etktf2erf2a75gbsrke2f1fm.apps.googleusercontent.com',
-  googleClientSecret: '7YyS87MYUHxwiVdoKPz4i2yX',
-  mongoURI: 'mongodb://dbuser:password@ds035796.mlab.com:35796/emailapp-dev',
-  cookieKey: 'gyfefghafhjgvxzC7riweyfoncmbfvuirbvjdkvnIEHRTQhdgvkahjgf'
-};
