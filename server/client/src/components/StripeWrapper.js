4bc3a4ad0681261ef4331e3fdf393b6c6a44a7ff
kayleabox <kayleabox09@gmail.com>
2018-04-22 22:18:36 -0500

    console logging publishableKey to check value on heroku

--- server/client/src/components/StripeWrapper.js
+++ server/client/src/components/StripeWrapper.js
@@ -5,6 +5,7 @@ import * as actions from '../actions';
 
 class StripeWrapper extends Component {
   render() {
+    console.log(process.env.REACT_APP_STRIPE_KEY);
     return (
       <StripeCheckout
         name='Email App'
